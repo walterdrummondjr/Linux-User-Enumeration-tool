25001b0a3805af8c40485b9b1e4419b8cf0181cc
Walter Drummond Jr <67807359+walterdrummondjr@users.noreply.github.com>
2026-06-30 16:44:21 -0400

    Update userenum.py

--- userenum.py
+++ userenum.py
@@ -5,13 +5,21 @@ import logging
 
 
 
-# List of commands that will run
-commands = "echo '\\033[94mBegnining........ \\033[0m' && id && who && whoami && groups && echo $PATH && grep -e /bin/bash /etc/passwd && lastlog && echo '\\033[92mEnumeration complete\\033[0m'"
+# List of commands that will run:
+# 1st command: id   - Shows the users id, group id and group membership
+# 2nd command: who   - Shows who is currenlty logged in
+# 3rd command: whoami - Shows who you are logged in as
+# 4th command: groups - Shows all groups for the current logged in user
+# 5th command: echo $PATH - Shows which directory the current user has access to
+# 6th command: grep -e /bin/bash /etc/passwd - Shows any users assigned to the Bash shell
+# 7th command: lastlogin Shows all user’s login name, port, and last login time
+
+commands = "echo '\\033[94mBegnining........ \\033[0m' && id && whoami && groups && echo $PATH && grep -e /bin/bash /etc/passwd && lastlog && echo '\\033[92mEnumeration complete....... \\033[0m'"
 
 
 #Run commands and displays output to the terminal
 output = subprocess.run(commands, shell=True, capture_output=True, text=True)
 
 
-
+# Displays commmands output to the terminal
 print(output.stdout)
