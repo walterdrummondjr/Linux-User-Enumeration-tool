ffc746644431a3e622cbc4d977ea1794409d9b7f
Walter Drummond Jr <67807359+walterdrummondjr@users.noreply.github.com>
2026-06-30 16:48:53 -0400

    Update userenum.py

--- userenum.py
+++ userenum.py
@@ -6,13 +6,12 @@ import logging
 
 
 # List of commands that will run:
-# 1st command: id   - Shows the users id, group id and group membership
-# 2nd command: who   - Shows who is currenlty logged in
-# 3rd command: whoami - Shows who you are logged in as
-# 4th command: groups - Shows all groups for the current logged in user
-# 5th command: echo $PATH - Shows which directory the current user has access to
-# 6th command: grep -e /bin/bash /etc/passwd - Shows any users assigned to the Bash shell
-# 7th command: lastlogin Shows all user’s login name, port, and last login time
+# 1st command: id  - Shows the users id, group id and group membership
+# 2nd command: whoami - Shows who you are logged in as
+# 3rd command: groups - Shows all groups for the current logged in user
+# 4th command: echo $PATH - Shows which directory the current user has access to
+# 5th command: grep -e /bin/bash /etc/passwd - Shows any users assigned to the Bash shell
+# 6th command: lastlogin - Shows all user’s login name, port, and last login time
 
 commands = "echo '\\033[94mBegnining........ \\033[0m' && id && whoami && groups && echo $PATH && grep -e /bin/bash /etc/passwd && lastlog && echo '\\033[92mEnumeration complete....... \\033[0m'"
 
