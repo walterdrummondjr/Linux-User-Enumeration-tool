123f7b568601dfd02f70f8236e8ae044c1618fca
Walter Drummond Jr <67807359+walterdrummondjr@users.noreply.github.com>
2026-06-30 12:31:13 -0400

    Update userenum.py

--- userenum.py
+++ userenum.py
@@ -6,7 +6,7 @@ import logging
 
 
 # List of commands that will run
-commands = "echo '\\033[94mBegnining........ \\033[0m' && ""id && who && whoami && echo $PATH &&""grep -i '/bin/.*sh' /etc/passwd && "" getent passwd | column -t -s: && ""echo '\\033[92mEnumeration complete\\033[0m'"
+commands = "echo '\\033[94mBegnining........ \\033[0m' && ""id && who && whoami && echo $PATH &&""grep '/home' /etc/passwd && "" getent passwd | column -t -s: && ""echo '\\033[92mEnumeration complete\\033[0m'"
 
 
 #Run commands and displays output to the terminal
